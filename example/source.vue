05e22054ced1421846116a4821d91dd17b9dfe91
Francisco <cbo317110@gmail.com>
2018-05-03 17:55:02 -0300

    working attempt

--- example/source.vue
+++ example/source.vue
@@ -4,11 +4,31 @@ div(ok)
 </template>
 
 <script>
+import requests from 'marengo-requests'
 export default {
+	marengo: {
+		env: {},
+		kext: {
+			requests
+		},
+		body: {
+			requests: {
+				search: {
+					base: 'https://www.instagram.com/web/search/topsearch/?context=blended&query=',
+					acceptor: {
+						purpose: res => res.data
+					}
+				}
+			}
+		}
+	},
   data () {
     return {
       msg: 'fesfff'
     }
+  },
+  mounted() {
+  	this.$requests('search').get('salah').purpose(console.log).fire()
   }
 }
 </script>
